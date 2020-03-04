6499a36a50d61ec2e285ce358108f5970c39891d
Neciu Dan <dan@adoreme.com>
2020-03-04 10:22:35 +0200

    working on store

--- src/store/actions.js
+++ src/store/actions.js
@@ -37,54 +37,54 @@ export const productActions = {
   },
   login({commit}, payload) {
     axios.post(`${API_BASE}/login`, payload).then(response => {
-     commit(LOGIN, response.data);
-    })
-  },
-  register({commit}, payload) {
-    axios.post(`${API_BASE}/register`, payload).then(response => {
-     commit(REGISTER, response.data);
-    })
-  },
-  updateProfile({commit}, payload) {
-    axios.post(`${API_BASE}/update-profile`, payload).then(response => {
-      commit(UPDATE_PROFILE, response.data);
-     })
-  },
-  allProducts ({commit}) {
-    commit(ALL_PRODUCTS)
-    // Fetch actual products from the API
-    axios.get(`${API_BASE}/products`).then(response => {
-      commit(ALL_PRODUCTS_SUCCESS, response.data)
-    })
-  },
-  productById ({commit}, payload) {
-    commit(PRODUCT_BY_ID)
-    // Fetch product by ID from API
-    axios.get(`${API_BASE}/products/${payload}`).then(response => {
-      commit(PRODUCT_BY_ID_SUCCESS, response.data)
-    })
-  },
-  addProduct ({commit}, payload) {
-    commit(ADD_PRODUCT)
-    // Create a new product via API
-    axios.post(`${API_BASE}/products`, payload).then(response => {
-      commit(ADD_PRODUCT_SUCCESS, response.data)
-    })
-  },
-  updateProduct ({commit}, payload) {
-    commit(UPDATE_PRODUCT)
-    // Update product via API
-    axios.put(`${API_BASE}/products/${payload._id}`, payload).then(response => {
-      commit(UPDATE_PRODUCT_SUCCESS, response.data)
-    })
-  },
-  removeProduct ({commit}, payload) {
-    commit(REMOVE_PRODUCT)
-    // Delete product via API
-    axios.delete(`${API_BASE}/products/${payload}`, payload).then(response => {
-      console.debug('response', response.data)
-      commit(REMOVE_PRODUCT_SUCCESS, response.data)
-    })
-  }
+    commit(LOGIN, response.data);
+  })
+},
+register({commit}, payload) {
+  axios.post(`${API_BASE}/register`, payload).then(response => {
+  commit(REGISTER, response.data);
+})
+},
+updateProfile({commit}, payload) {
+  axios.post(`${API_BASE}/update-profile`, payload).then(response => {
+    commit(UPDATE_PROFILE, response.data);
+  })
+},
+allProducts ({commit}) {
+  commit(ALL_PRODUCTS)
+  // Fetch actual products from the API
+  axios.get(`${API_BASE}/products`).then(response => {
+    commit(ALL_PRODUCTS_SUCCESS, response.data)
+  })
+},
+productById ({commit}, payload) {
+  commit(PRODUCT_BY_ID)
+  // Fetch product by ID from API
+  axios.get(`${API_BASE}/products/${payload}`).then(response => {
+    commit(PRODUCT_BY_ID_SUCCESS, response.data)
+  })
+},
+addProduct ({commit}, payload) {
+  commit(ADD_PRODUCT)
+  // Create a new product via API
+  axios.post(`${API_BASE}/products`, payload).then(response => {
+    commit(ADD_PRODUCT_SUCCESS, response.data)
+  })
+},
+updateProduct ({commit}, payload) {
+  commit(UPDATE_PRODUCT)
+  // Update product via API
+  axios.put(`${API_BASE}/products/${payload._id}`, payload).then(response => {
+    commit(UPDATE_PRODUCT_SUCCESS, response.data)
+  })
+},
+removeProduct ({commit}, payload) {
+  commit(REMOVE_PRODUCT)
+  // Delete product via API
+  axios.delete(`${API_BASE}/products/${payload}`, payload).then(response => {
+    console.debug('response', response.data)
+    commit(REMOVE_PRODUCT_SUCCESS, response.data)
+  })
+}
 };
 
